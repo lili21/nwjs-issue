e07574282d5a36c89ba2e1d92cf64b8b3ecaf73e
li.li <li.li@ele.me>
2017-05-17 17:05:14 +0800

    update menu

--- src/inject-menu.js
+++ src/inject-menu.js
@@ -1,12 +1,6 @@
 var win = nw.Window.get()
 var menu = new nw.Menu()
 
-// menu.append(new nw.MenuItem({
-//   label: '',
-//   click: function () {
-//     history.back()
-//   }
-// }))
 menu.append(new nw.MenuItem({
   label: 'Reload',
   click: function () {
@@ -14,48 +8,8 @@ menu.append(new nw.MenuItem({
   }
 }))
 
-var copyitem = new nw.MenuItem({
-  label: '复制',
-  click: function () {
-    try {
-      document.execCommand('copy')
-    } catch (e) {
-      console.error(e)
-    }
-  }
-})
-
-var pasteitem = new nw.MenuItem({
-  label: '粘贴',
-  click: function () {
-    try {
-      document.execCommand('paste')
-    } catch (e) {
-      console.error(e)
-    }
-  }
-})
-
-menu.has = function (_item) {
-  return this.items
-    .filter(item => item.label === _item.label)
-    .length !== 0
-}
-
 document.body.addEventListener('contextmenu', function (ev) {
   ev.preventDefault()
-  var select = document.getSelection().toString()
-  if (select) {
-    !menu.has(copyitem) && menu.append(copyitem)
-  } else {
-    menu.has(copyitem) && menu.remove(copyitem)
-  }
-
-  if (editable(ev.target)) {
-    !menu.has(pasteitem) && menu.append(pasteitem)
-  } else {
-    menu.has(pasteitem) && menu.remove(pasteitem)
-  }
 
   if (window === window.parent) {
     menu.popup(ev.x, ev.y)
@@ -67,9 +21,3 @@ document.body.addEventListener('contextmenu', function (ev) {
   }
   return false
 }, false)
-
-function editable (ele) {
-  return ele instanceof HTMLInputElement ||
-    ele instanceof HTMLTextAreaElement ||
-    ele.isContentEditable
-}
